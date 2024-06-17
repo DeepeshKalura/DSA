6823c495d219ac81f5a0cb451b248124fbe43764
DeepeshKalura <deepeshkalurs@gmail.com>
2024-06-17 09:30:23 +0530

    feat: Implement palindrome partitioning algorithm
    This commit implements the palindrome partitioning algorithm in the `partition` method of the `Solution` class. The algorithm uses recursion to find all possible palindrome partitions of a given string `s`. It checks if each substring is a palindrome and adds it to the result list if it is. The algorithm has been tested and is working correctly.

--- daily/131.palindrome-partitioning.py
+++ daily/131.palindrome-partitioning.py
@@ -9,14 +9,27 @@ from typing import List
 
 # TODO: Can't do yet still look up solution and feel this greedy basdard
 # TODO: Recurssion is damm hard
+# TODO: Still We want to dance
 
 class Solution:
 
     
     def partition(self, s: str) -> List[List[str]]:
-        
+        result = []
 
-        pass
+        def recursion(index, value:List):
+            if(len(s) == index):
+                result.append(value)
+                return 
+            
+            for end in range(index+1,len(s) + 1 ):
+                print(f"{index}, {end}")
+                print(s[index: end])
+                if(s[index: end] == s[index: end][::-1]):
+                    recursion(end, value+[s[index: end]])
+
+        recursion(0, [])
+        return result
 
         
 # @lc code=end
