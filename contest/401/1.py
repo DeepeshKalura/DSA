f9b4a81ce19d54b7f4f1f00e2fde559f1677a52b
DeepeshKalura <deepeshkalurs@gmail.com>
2024-06-26 12:10:15 +0530

    remove contest from project

--- contest/401/1.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def numberOfChild(self, n: int, k: int) -> int:
-        cycle = 2 * (n - 1)
-        remainder = k % cycle
-        if remainder < n:
-            return remainder
-        else:
-            return cycle - remainder
